6bb48784fc9308a99c92eeff00eda510b114ad2d
mook-jp <mook24.jp@gmail.com>
2025-04-06 22:43:16 +0900

    update godoc

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
-/*
-Copyright © 2025 mook-jp <mook24.jp@gmail.com>
+// The MIT License (MIT)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+// Copyright © 2025 mook-jp <mook24.jp@gmail.com>
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 package main
 
 import "github.com/mook-jp/go-practice/cmd"
@@ -27,10 +27,10 @@ func main() {
 	cmd.Execute()
 }
 
-// 追加予定の機能
-// 2.フォルダの最初の文字を変更する処理(gp -> any)
-// - replaceHead Option
-// 3.3桁のフォルダ名に対応するコマンド
-// - replaceDigits Option
-// 文字数で取得したエラーになる
-// gp5とかになっていた時にエラーになってしまう
+// TODO:親フォルダを指定可能にする
+// TODO:フォルダの最初の文字を変更する処理(gp -> any)
+// 		-> replaceHead Option
+// TODO:3桁のフォルダ名に対応するコマンド
+// TODO:replaceDigits Option
+// TODO:文字数で取得したエラーになる
+//      -> gp5とかになっていた時にエラーになってしまう
